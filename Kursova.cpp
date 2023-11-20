03eeef82600edb40e13c38c011b4e47b4bfefb84
MarMykhailo <mykhailo.marochkanych.pz.2022@lpnu.ua>
2023-11-20 12:25:58 +0200

    Впорядкував все що пов'язане з видаленням і додаванням елементів

--- Kursova.cpp
+++ Kursova.cpp
@@ -177,4 +177,310 @@ System::Void KursovaOOP::Kursova::tsmiSImport_Click(System::Object^ sender, Syst
     return System::Void();
 }
 
+System::Void KursovaOOP::Kursova::tlpTable_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	// Перевірка, чи подія відноситься до клікання мишкою
+	if (e->GetType() == System::Windows::Forms::MouseEventArgs::typeid) {
+		System::Windows::Forms::MouseEventArgs^ mouseEvent = (System::Windows::Forms::MouseEventArgs^)e;
+
+		// Перевірка, чи клацнуто правою кнопкою миші
+		if (mouseEvent->Button == System::Windows::Forms::MouseButtons::Right) {
+			// Ваш код для відображення контекстного меню
+			cmsMenu->Show(tlpTable, mouseEvent->Location);
+		}
+	}
+	return System::Void();
+}
+
+System::Void KursovaOOP::Kursova::cmsDelete_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	// Беремо текст з текстового поля
+	String^ deleteIndexText = cmstbDelete->Text;
+
+	// Перевірка на пустий рядок
+	if (String::IsNullOrEmpty(deleteIndexText)) {
+		MessageBox::Show("Please enter an index to delete.", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		return;
+	}
+
+	// Перевірка на коректний формат числа
+	int deleteIndex;
+	if (!Int32::TryParse(deleteIndexText, deleteIndex)) {
+		MessageBox::Show("Invalid index. Please enter a valid integer.", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		return;
+	}
+
+	// Перевірка на допустимий діапазон індексів
+	if (deleteIndex < 0 || deleteIndex >= songManager->songList.getSize()) {
+		MessageBox::Show("Index out of range. Please enter a valid index.", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		return;
+	}
+
+	// Видалення пісні та оновлення таблиці
+	songManager->songList.removeAt(deleteIndex + 1);
+	UpdateTable();
+	return System::Void();
+}
+
+System::Void KursovaOOP::Kursova::tlpTable_PreviewKeyDown(System::Object^ sender, System::Windows::Forms::PreviewKeyDownEventArgs^ e)
+{
+    if (e->KeyCode == Keys::Enter) {
+        MessageBox::Show("Enter");
+    }
+    return System::Void();
+}
+
+System::Void KursovaOOP::Kursova::cmsAdd_Click(System::Object^ sender, System::EventArgs^ e)
+{
+
+	Song* song = new Song();
+
+	// Дані про виконавців
+	if (!String::IsNullOrEmpty(cmstbSongers->Text))
+	{
+		std::vector<std::string> songers;
+		// Виконавці розділені комою
+		std::string songersString = msclr::interop::marshal_as<std::string>(cmstbSongers->Text);
+		std::string delimiter = ",";
+		size_t pos = 0;
+		std::string token;
+		while ((pos = songersString.find(delimiter)) != std::string::npos) {
+			token = songersString.substr(0, pos);
+			songers.push_back(token);
+			songersString.erase(0, pos + delimiter.length());
+		}
+		// Додаємо залишок рядка після останньої коми (або весь рядок, якщо ком не знайдено)
+		songers.push_back(songersString);
+		song->setSongers(songers);
+	}
+
+	// Дані про назву
+	if (!String::IsNullOrEmpty(cmstbName->Text))
+	{
+		std::string name = msclr::interop::marshal_as<std::string>(cmstbName->Text);
+		song->setName(name);
+	}
+
+	// Дані про альбом
+	if (!String::IsNullOrEmpty(cmstbAlbom->Text))
+	{
+		std::string albom = msclr::interop::marshal_as<std::string>(cmstbAlbom->Text);
+		song->setAlbom(albom);
+	}
+
+	// Дані про рік
+	if (!String::IsNullOrEmpty(cmstbYear->Text))
+	{
+		int year;
+		if (Int32::TryParse(cmstbYear->Text, year)) {
+			song->setYear(year);
+		}
+		else {
+			// Обробка помилки, наприклад, виведення повідомлення користувачу або інше необхідне дійство
+			// Наприклад, MessageBox::Show("Введіть правильний рік");
+			return;
+		}
+	}
+
+	// Дані про формат
+	if (!String::IsNullOrEmpty(cmstbFormat->Text))
+	{
+		std::string format = msclr::interop::marshal_as<std::string>(cmstbFormat->Text);
+		song->setFormat(format);
+	}
+
+	// Дані про розмір
+	if (!String::IsNullOrEmpty(cmstbSize->Text))
+	{
+		int size;
+		if (Int32::TryParse(cmstbSize->Text, size)) {
+			song->setSize(size);
+		}
+		else {
+			// Обробка помилки, наприклад, виведення повідомлення користувачу або інше необхідне дійство
+			// Наприклад, MessageBox::Show("Введіть правильний розмір");
+			return;
+		}
+	}
+
+	// Дані про зарубіжність
+	if (!String::IsNullOrEmpty(cmstbIsImport->Text))
+	{
+		std::wstring isImport = msclr::interop::marshal_as<std::wstring>(cmstbIsImport->Text);
+		if (isImport == L"Так") {
+			song->setIsImport(true);
+		}
+		else
+		{
+			song->setIsImport(false);
+		}
+	}
+
+	songManager->songList.push_front(*song);
+	UpdateTable();
+    return System::Void();
+}
+
+//private: System::Void cmsAdd_Click(System::Object^ sender, System::EventArgs^ e) {
+//	// Створіть нову форму (вікно)
+//	Form^ newForm = gcnew Form();
+//	newForm->Text = L"Нове вікно";
+//
+//	//заблокувати розміри вікна
+//	newForm->MaximizeBox = false;
+//	newForm->MinimizeBox = false;
+//	newForm->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+//
+//	newForm->Width = 250;
+//	newForm->Height = 325;
+//	// Додайте лейбли і текстові поля для виконавця, назви, альбому, року, формату, розміру та зарубіжності
+//	Label^ label1 = gcnew Label();
+//	label1->Text = L"Виконавець:";
+//	label1->Location = System::Drawing::Point(10, 30);
+//	newForm->Controls->Add(label1);
+//
+//	TextBox^ textBox1 = gcnew TextBox();
+//	textBox1->Location = System::Drawing::Point(125, 30);
+//	newForm->Controls->Add(textBox1);
+//
+//	Label^ label2 = gcnew Label();
+//	label2->Text = L"Назва:";
+//	label2->Location = System::Drawing::Point(10, 60);
+//	newForm->Controls->Add(label2);
+//
+//	TextBox^ textBox2 = gcnew TextBox();
+//	textBox2->Location = System::Drawing::Point(125, 60);
+//	newForm->Controls->Add(textBox2);
+//
+//	Label^ label3 = gcnew Label();
+//	label3->Text = L"Альбом:";
+//	label3->Location = System::Drawing::Point(10, 90);
+//	newForm->Controls->Add(label3);
+//
+//	TextBox^ textBox3 = gcnew TextBox();
+//	textBox3->Location = System::Drawing::Point(125, 90);
+//	newForm->Controls->Add(textBox3);
+//
+//	Label^ label4 = gcnew Label();
+//	label4->Text = L"Рік:";
+//	label4->Location = System::Drawing::Point(10, 120);
+//	newForm->Controls->Add(label4);
+//
+//	TextBox^ textBox4 = gcnew TextBox();
+//	textBox4->Location = System::Drawing::Point(125, 120);
+//	newForm->Controls->Add(textBox4);
+//
+//	Label^ label5 = gcnew Label();
+//	label5->Text = L"Формат:";
+//	label5->Location = System::Drawing::Point(10, 150);  
+//	newForm->Controls->Add(label5);
+//
+//	TextBox^ textBox5 = gcnew TextBox();
+//	textBox5->Location = System::Drawing::Point(125, 150);
+//	newForm->Controls->Add(textBox5);
+//
+//
+//	Label^ label6 = gcnew Label();
+//	label6->Text = L"Розмір:";
+//	label6->Location = System::Drawing::Point(10, 180);
+//	newForm->Controls->Add(label6);
+//
+//	TextBox^ textBox6 = gcnew TextBox();
+//	textBox6->Location = System::Drawing::Point(125, 180);
+//	newForm->Controls->Add(textBox6);
+//
+//	// Додайте чекбокс
+//	CheckBox^ newCheckBox = gcnew CheckBox();
+//	newCheckBox->Text = L"Зарубіжна";
+//	newCheckBox->Location = System::Drawing::Point(75, 210);
+//	newForm->Controls->Add(newCheckBox);
+//
+//	// Створіть нову кнопку "Додати"
+//	Button^ addButton = gcnew Button();
+//	addButton->Text = L"Додати";
+//	addButton->Location = System::Drawing::Point(25, 240);
+//	addButton->Click += gcnew System::EventHandler(this, &Kursova::btnAdd_Click);
+//	newForm->Controls->Add(addButton);
+//
+//	// Створіть нову кнопку "Скасувати"
+//	Button^ cancelButton = gcnew Button();
+//	cancelButton->Text = L"Скасувати";
+//	cancelButton->Location = System::Drawing::Point(125, 240);
+//	cancelButton->Click += gcnew System::EventHandler(this, &Kursova::btnCancel_Click);
+//	newForm->Controls->Add(cancelButton);
+//
+//	// Покажіть нове вікно
+//	newForm->Show();
+//}
+//
+
+//private: System::Void cmsDelete_Click(System::Object^ sender, System::EventArgs^ e) {
+//	// Отримуємо елемент, на якому було клікнуто
+//	Control^ clickedControl = tlpTable->GetChildAtPoint(tlpTable->PointToClient(Cursor->Position));
+//
+//	// Перевірка, чи це Label
+//	Label^ clickedLabel = dynamic_cast<Label^>(clickedControl);
+//
+//	// Видаліть елемент (Label) з TableLayoutPanel
+//	if (clickedLabel != nullptr) {
+//		// Отримайте індекс рядка
+//		int row = tlpTable->GetRow(clickedLabel);
+//		MessageBox::Show("DELETE");
+//
+//
+//		songManager->songList.removeAt(row);
+//
+//		// Оновіть відображення списку пісень
+//		UpdateTable();
+//	}
+//}
+
+//private: System::Void tlpTable_Click(System::Object^ sender, System::EventArgs^ e) {
+//	// Перевірка, чи подія відноситься до клікання мишкою
+//	if (e->GetType() == System::Windows::Forms::MouseEventArgs::typeid) {
+//		System::Windows::Forms::MouseEventArgs^ mouseEvent = (System::Windows::Forms::MouseEventArgs^)e;
+//
+//		// Перевірка, чи клацнуто правою кнопкою миші
+//		if (mouseEvent->Button == System::Windows::Forms::MouseButtons::Right) {
+//			// Ваш код для відображення контекстного меню
+//			cmsMenu->Show(tlpTable, mouseEvent->Location);
+//		}
+//		else if (mouseEvent->Button == System::Windows::Forms::MouseButtons::Left) {
+//			// Змінення фону всіх лейблів на білий перед тим, як підсвічувати поточний рядок
+//			for (int i = 0; i < tlpTable->ColumnCount; i++) {
+//				for (int j = 0; j < tlpTable->RowCount; j++) {
+//					Control^ labelInTable = tlpTable->GetControlFromPosition(i, j);
+//					if (labelInTable != nullptr && labelInTable->GetType() == Label::typeid) {
+//						labelInTable->BackColor = System::Drawing::Color::White;
+//					}
+//				}
+//			}
+//
+//			// Визначення елемента, на якому було клікнуто
+//			Control^ clickedControl = tlpTable->GetChildAtPoint(tlpTable->PointToClient(mouseEvent->Location));
+//
+//			// Перевірка, чи це Label
+//			if (clickedControl != nullptr && clickedControl->GetType() == Label::typeid) {
+//				// Отримайте індекс рядка та стовпця
+//				int row = tlpTable->GetRow(clickedControl);
+//				int column = tlpTable->GetColumn(clickedControl);
+//
+//				// Змініть колір фону лейблу на синій
+//				clickedControl->BackColor = System::Drawing::Color::Blue;
+//
+//				// Змініть колір фону всіх лейблів у поточному рядку
+//				for (int i = 0; i < tlpTable->ColumnCount; i++) {
+//					Control^ labelInCurrentRow = tlpTable->GetControlFromPosition(i, row);
+//					if (labelInCurrentRow != nullptr && labelInCurrentRow->GetType() == Label::typeid) {
+//						labelInCurrentRow->BackColor = System::Drawing::Color::Blue;
+//					}
+//				}
+//
+//				// Ваш код для обробки кліку на Label у конкретному рядку та стовпці
+//				this->bSearch->Text = " " + (row + 1).ToString() + " ";
+//			}
+//		}
+//	}
+//}
+
 
